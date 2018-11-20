3aab0714fbd17fff3abdc91e6017675f4cbd31bd
L. Scott Hudson <LHudson73@gmail.com>
2018-11-20 03:59:50 +0000

    compile Gala.Dolly UI application

--- Galatea.AI.Language/Properties/AssemblyInfo.cs
+++ Galatea.AI.Language/Properties/AssemblyInfo.cs
@@ -10,7 +10,6 @@ using System.Resources;
 [assembly: AssemblyDescription("Galatea AI language models and localized resources.")]
 [assembly: AssemblyConfiguration("")]
 
-/*
 // Galatea assemblies
 [assembly: InternalsVisibleTo("Gala.Dolly.Speech, PublicKey=" +
     "00240000048000009400000006020000002400005253413100040000010001006bff5ef396f62b" +
@@ -18,4 +17,4 @@ using System.Resources;
     "f2b1331392044f1fee5c00bd3712048c5375957eb6c97347621128c5c99d7516f1992c5925a267" +
     "6e69ff838044bcdc5e64e66bc6dd10431a23c63b625ff2c2dba1fcc11ce983fd03dcd436622385" +
     "81668ab6")]
- */
+
